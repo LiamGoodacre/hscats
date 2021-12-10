3910c68166c96a1bd0589c92e83492fd87361a77
Liam Goodacre <goodacre.liam@gmail.com>
2021-12-10 23:33:15 +0000

    monad syntax, nasty unicode symbols, yoneda embeddings, crash compiler, etc

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,15 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImplicitParams #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
@@ -19,7 +22,6 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
-{-# LANGUAGE RebindableSyntax #-}
 {-# OPTIONS_GHC -Wall -Werror -Wextra #-}
 
 module Main where
@@ -29,17 +31,17 @@ import Data.Proxy
 import Prelude
   ( Either (..),
     IO,
+    Int,
     Monoid (..),
+    Num (..),
     Semigroup ((<>)),
     either,
+    fromInteger,
     fst,
     id,
     putStrLn,
     snd,
     (.),
-    Int,
-    fromInteger,
-    Num (..),
   )
 
 data Dict c where Dict :: c => Dict c
@@ -50,35 +52,38 @@ type CAT i = i -> i -> Type
 type FUNCTOR :: forall i j. CAT i -> CAT j -> Type
 type FUNCTOR d c = Proxy d -> Proxy c -> Type
 
+type ENDO :: CAT i -> Type
+type ENDO k = FUNCTOR k k
+
 type DOM :: forall i (d :: CAT i) c. FUNCTOR d c -> CAT i
 type DOM (f :: FUNCTOR d c) = d
 
 type COD :: forall j d (c :: CAT j). FUNCTOR d c -> CAT j
 type COD (f :: FUNCTOR d c) = c
 
-type Obj :: forall i. CAT i -> i -> Constraint
-type family Obj c x = o | o -> c x
+type (∈) :: forall i. i -> CAT i -> Constraint
+type family x ∈ k = o | o -> k x
 
 type Cat :: CAT i -> Constraint
 class Cat k where
-  identity :: Obj k i => k i i
-  (∘) :: (Obj k a, Obj k b, Obj k x) => k a b -> k x a -> k x b
+  identity :: i ∈ k => k i i
+  (∘) :: (a ∈ k, b ∈ k, x ∈ k) => k a b -> k x a -> k x b
 
 type Act :: FUNCTOR (d :: CAT i) (c :: CAT j) -> i -> j
 type family Act f o
 
-class (Obj (COD f) (Act f o)) => Acts f o
+class (Act f o ∈ COD f) => Acts f o
 
-instance (Obj (COD f) (Act f o)) => Acts f o
+instance (Act f o ∈ COD f) => Acts f o
 
 type Functorial :: FUNCTOR (d :: CAT o) c -> o -> Constraint
-type Functorial f o = (Obj (DOM f) o => Acts f o)
+type Functorial f o = (o ∈ DOM f => Acts f o)
 
 type Functor :: FUNCTOR d c -> Constraint
 class (Cat (DOM f), Cat (COD f), forall o. Functorial f o) => Functor f where
   map_ ::
-    ( Obj (DOM f) a,
-      Obj (DOM f) b
+    ( a ∈ DOM f,
+      b ∈ DOM f
     ) =>
     DOM f a b ->
     COD f (Act f a) (Act f b)
@@ -86,51 +91,26 @@ class (Cat (DOM f), Cat (COD f), forall o. Functorial f o) => Functor f where
 map ::
   forall f a b.
   ( Functor f,
-    Obj (DOM f) a,
-    Obj (DOM f) b
+    a ∈ DOM f,
+    b ∈ DOM f
   ) =>
   DOM f a b ->
   COD f (Act f a) (Act f b)
 map d = map_ @_ @_ @f d
 
-type TYPE :: CAT Type
-type TYPE = (->)
 
-type instance Obj TYPE t = (t ~ t, TYPE ~ TYPE)
+data Id :: FUNCTOR i i
+type instance Act Id x = x
+instance Cat i => Functor (Id :: FUNCTOR i i) where
+  map_ = id
 
-instance Cat TYPE where
-  identity = id
-  (∘) = (.)
-
-data READER :: Type -> FUNCTOR TYPE TYPE
-type instance Act (READER x) y = x -> y
-instance Functor (READER x) where
-  map_ = (.)
-
-data ENV :: Type -> FUNCTOR TYPE TYPE
-type instance Act (ENV x) y = (x, y)
-instance Functor (ENV x) where
-  map_ f (l, r) = (l, f r)
-
-data (∘∘) :: FUNCTOR a b -> FUNCTOR x a -> FUNCTOR x b
-type instance Act (f ∘∘ g) x = Act f (Act g x)
-instance (Functor f, Functor g) => Functor (f ∘∘ g) where
+data (∘) :: FUNCTOR a b -> FUNCTOR x a -> FUNCTOR x b
+type instance Act (f ∘ g) x = Act f (Act g x)
+instance (Functor f, Functor g) => Functor (f ∘ g) where
   map_ = map @f . map @g
 
-data MONOID :: Type -> CAT () where
-  MONOID :: m -> MONOID m '() '()
-
-type instance Obj (MONOID m) x =
-  ( x ~ '(),
-    MONOID m ~ MONOID m
-  )
-
-instance Monoid m => Cat (MONOID m) where
-  identity = MONOID mempty
-  MONOID l ∘ MONOID r = MONOID (l <> r)
-
-data (××) :: CAT s -> CAT t -> CAT (s, t) where
-  (:××:) :: l a b -> r x y -> (l ×× r) '(a, x) '(b, y)
+data (×) :: CAT s -> CAT t -> CAT (s, t) where
+  (:×:) :: l a b -> r x y -> (l × r) '(a, x) '(b, y)
 
 type Fst :: (l, r) -> l
 type family Fst p where
@@ -141,133 +121,132 @@ type family Snd p where
   Snd '(a, b) = b
 
 type instance
-  Obj (l ×× r) v =
+  v ∈ (l × r) =
     ( v ~ '(Fst v, Snd v),
-      Obj l (Fst v),
-      Obj r (Snd v),
-      (l ×× r) ~ (l ×× r)
+      Fst v ∈ l,
+      Snd v ∈ r,
+      (l × r) ~ (l × r)
     )
 
-instance (Cat l, Cat r) => Cat (l ×× r) where
-  identity = identity :××: identity
-  (a :××: b) ∘ (c :××: d) = (a ∘ c) :××: (b ∘ d)
+instance (Cat l, Cat r) => Cat (l × r) where
+  identity = identity :×: identity
+  (a :×: b) ∘ (c :×: d) = (a ∘ c) :×: (b ∘ d)
 
-data DELTA :: FUNCTOR k (k ×× k)
-type instance Act DELTA x = '(x, x)
-instance Cat k => Functor (DELTA :: FUNCTOR k (k ×× k)) where
-  map_ f = (f :××: f)
+data OP :: CAT i -> CAT i where OP :: k b a -> OP k a b
+type instance i ∈ OP k = (i ∈ k, OP k ~ OP k)
+instance Cat k => Cat (OP k) where
+  identity = OP identity
+  OP g ∘ OP f = OP (f ∘ g)
 
-data (∧) :: FUNCTOR (TYPE ×× TYPE) TYPE
-type instance Act (∧) x = (Fst x, Snd x)
-instance Functor (∧) where
-  map_ (f :××: g) (a, b) = (f a, g b)
+type TYPE = (->) :: CAT Type
+type instance t ∈ TYPE = (t ~ t, TYPE ~ TYPE)
+instance Cat TYPE where
+  identity = id
+  (∘) = (.)
 
-data (∨) :: FUNCTOR (TYPE ×× TYPE) TYPE
-type instance Act (∨) x = Either (Fst x) (Snd x)
-instance Functor (∨) where
-  map_ (f :××: g) = \case
-    Left a -> Left (f a)
-    Right b -> Right (g b)
+-- yoneda embeddings
+
+data Y :: forall (k :: CAT i) -> i -> FUNCTOR k TYPE
+type instance Act (Y k d) c = k d c
+instance (d ∈ k, Cat k) => Functor (Y k d) where
+  map_ = (∘)
+
+data K :: forall (k :: CAT i) -> i -> FUNCTOR (OP k) TYPE
+type instance Act (K k c) d = k d c
+instance (d ∈ k, Cat k) => Functor (K k d) where
+  map_ (OP g) f = f ∘ g
+
+data HOM :: forall (k :: CAT i) -> FUNCTOR (OP k × k) TYPE
+type instance Act (HOM k) o = k (Fst o) (Snd o)
+-- uncomment for panic - https://gitlab.haskell.org/ghc/ghc/-/issues/20231
+-- instance Cat k => Functor (HOM k) where
+--   map_ (f :×: g) t = f ∘ t ∘ g
 
 type Adjoint :: FUNCTOR c d -> FUNCTOR d c -> Constraint
 class (Functor f, Functor g) => Adjoint f (g :: FUNCTOR d c) where
-  leftAdjointed_ ::
+  leftAdjoint_ ::
     forall a b.
-    ( Obj c a,
-      Obj d b
+    ( a ∈ c,
+      b ∈ d
     ) =>
     c a (Act g b) ->
     d (Act f a) b
 
-  rightAdjointed_ ::
+  rightAdjoint_ ::
     forall a b.
-    ( Obj c a,
-      Obj d b
+    ( a ∈ c,
+      b ∈ d
     ) =>
     d (Act f a) b ->
     c a (Act g b)
 
-leftAdjointed ::
+leftAdjoint ::
   forall {c} {d} (f :: FUNCTOR c d) (g :: FUNCTOR d c) a b.
   ( Adjoint f g,
-    Obj c a,
-    Obj d b
+    a ∈ c,
+    b ∈ d
   ) =>
   c a (Act g b) ->
   d (Act f a) b
-leftAdjointed = leftAdjointed_ @c @d @f @g
+leftAdjoint = leftAdjoint_ @c @d @f @g
 
-rightAdjointed ::
+rightAdjoint ::
   forall {c} {d} (f :: FUNCTOR c d) (g :: FUNCTOR d c) a b.
   ( Adjoint f g,
-    Obj c a,
-    Obj d b
+    a ∈ c,
+    b ∈ d
   ) =>
   d (Act f a) b ->
   c a (Act g b)
-rightAdjointed = rightAdjointed_ @c @d @f @g
-
-instance Adjoint DELTA (∧) where
-  leftAdjointed_ t = (fst . t) :××: (snd . t)
-  rightAdjointed_ (f :××: g) = \x -> (f x, g x)
-
-instance Adjoint (∨) DELTA where
-  leftAdjointed_ (f :××: g) = f `either` g
-  rightAdjointed_ t = (t . Left) :××: (t . Right)
-
-type MONAD :: CAT i -> Type
-type MONAD k = FUNCTOR k k
-
-type COMONAD :: CAT i -> Type
-type COMONAD k = FUNCTOR k k
+rightAdjoint = rightAdjoint_ @c @d @f @g
 
 unit ::
   forall
     {c}
     {d}
-    (m :: MONAD c)
+    (m :: ENDO c)
     {f :: FUNCTOR c d}
     {g :: FUNCTOR d c}
     a.
-  ( m ~ (g ∘∘ f),
+  ( m ~ (g ∘ f),
     Adjoint f g,
-    Obj c a
+    a ∈ c
   ) =>
   c a (Act m a)
-unit = rightAdjointed @f @g identity
+unit = rightAdjoint @f @g identity
 
 counit ::
   forall
     {c}
     {d}
-    (w :: COMONAD d)
+    (w :: ENDO d)
     {f :: FUNCTOR c d}
     {g :: FUNCTOR d c}
     a.
-  ( w ~ (f ∘∘ g),
+  ( w ~ (f ∘ g),
     Adjoint f g,
-    Obj d a
+    a ∈ d
   ) =>
   d (Act w a) a
-counit = leftAdjointed @f @g identity
+counit = leftAdjoint @f @g identity
 
 join ::
   forall
     {i}
     {c :: CAT i}
     {d :: CAT i}
-    (m :: MONAD c)
+    (m :: ENDO c)
     {f :: FUNCTOR c d}
     {g :: FUNCTOR d c}
     a.
-  ( m ~ (g ∘∘ f),
+  ( m ~ (g ∘ f),
     Adjoint f g,
-    Obj c a
+    a ∈ c
   ) =>
-  c (Act (m ∘∘ m) a) (Act m a)
+  c (Act (m ∘ m) a) (Act m a)
 join = do
   let t :: d (Act f (Act g (Act f a))) (Act f a)
-      t = counit @(f ∘∘ g)
+      t = counit @(f ∘ g)
   map @g t
 
 extend ::
@@ -275,19 +254,76 @@ extend ::
     {i}
     {c :: CAT i}
     {d :: CAT i}
-    (w :: COMONAD d)
+    (w :: ENDO d)
     {f :: FUNCTOR c d}
     {g :: FUNCTOR d c}
     a.
-  ( w ~ (f ∘∘ g),
+  ( w ~ (f ∘ g),
     Adjoint f g,
-    Obj d a
+    a ∈ d
   ) =>
-  d (Act w a) (Act (w ∘∘ w) a)
+  d (Act w a) (Act (w ∘ w) a)
 extend = do
   let t :: c (Act g a) (Act g (Act f (Act g a)))
-      t = unit @(g ∘∘ f)
+      t = unit @(g ∘ f)
   map @f t
+
+---
+
+
+data READER :: Type -> FUNCTOR TYPE TYPE
+type instance Act (READER x) y = x -> y
+instance Functor (READER x) where
+  map_ = (.)
+
+
+data ENV :: Type -> FUNCTOR TYPE TYPE
+type instance Act (ENV x) y = (x, y)
+instance Functor (ENV x) where
+  map_ f (l, r) = (l, f r)
+
+
+data MONOID :: Type -> CAT () where
+  MONOID :: m -> MONOID m '() '()
+
+type instance x ∈ MONOID m =
+  ( x ~ '(),
+    MONOID m ~ MONOID m
+  )
+
+instance Monoid m => Cat (MONOID m) where
+  identity = MONOID mempty
+  MONOID l ∘ MONOID r = MONOID (l <> r)
+
+
+
+data DELTA :: forall (k :: CAT i) -> FUNCTOR k (k × k)
+type instance Act (DELTA k) x = '(x, x)
+instance Cat k => Functor (DELTA k) where
+  map_ f = (f :×: f)
+
+
+data (∧) :: FUNCTOR (TYPE × TYPE) TYPE
+type instance Act (∧) x = (Fst x, Snd x)
+instance Functor (∧) where
+  map_ (f :×: g) (a, b) = (f a, g b)
+
+
+data (∨) :: FUNCTOR (TYPE × TYPE) TYPE
+type instance Act (∨) x = Either (Fst x) (Snd x)
+instance Functor (∨) where
+  map_ (f :×: g) = \case
+    Left a -> Left (f a)
+    Right b -> Right (g b)
+
+
+instance Adjoint (DELTA TYPE) (∧) where
+  leftAdjoint_ t = (fst . t) :×: (snd . t)
+  rightAdjoint_ (f :×: g) = \x -> (f x, g x)
+
+instance Adjoint (∨) (DELTA TYPE) where
+  leftAdjoint_ (f :×: g) = f `either` g
+  rightAdjoint_ t = (t . Left) :×: (t . Right)
 
 stateFlatMap ::
   forall s a b.
@@ -296,11 +332,11 @@ stateFlatMap ::
   Act (STATE s) b
 stateFlatMap m f = join @(STATE s) (map @(STATE s) f m)
 
-type STATE s = (READER s) ∘∘ (ENV s)
+type STATE s = READER s ∘ ENV s
 
 instance Adjoint (ENV s) (READER s) where
-  leftAdjointed_ asb (s, a) = asb a s
-  rightAdjointed_ sab a s = sab (s, a)
+  leftAdjoint_ asb (s, a) = asb a s
+  rightAdjoint_ sab a s = sab (s, a)
 
 get :: Act (STATE s) s
 get s = (s, s)
@@ -311,13 +347,24 @@ put v _ = (v, ())
 modify :: (s -> s) -> Act (STATE s) ()
 modify t s = (t s, ())
 
+data Do m = Do
+  (forall a b. Act m a -> (a -> Act m b) -> Act m b)
+  (forall a. a -> Act m a)
+
+(>>=) :: forall m a b. (?monad :: Do m) => Act m a -> (a -> Act m b) -> Act m b
+(>>=) = let Do f _ =  ?monad in f @a @b
+
+pure :: forall m a. (?monad :: Do m) => a -> Act m a
+pure = let Do _ u = ?monad in u
+
+stateMonad :: ((?monad :: Do (STATE s)) => r) -> r
+stateMonad t = let ?monad = Do stateFlatMap (unit @(STATE _)) in t
+
 postinc :: Act (STATE Int) Int
-postinc = do
+postinc = stateMonad do
   x <- get
   _ <- put (x + 1)
   pure x
-  where (>>=) = stateFlatMap
-        pure = unit @(STATE _)
 
 -- f ∘ g ~> h
 -- f ~> h / g
