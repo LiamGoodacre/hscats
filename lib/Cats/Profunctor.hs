261d07c35aa72f14772369f3e03b1c5f1863fb0a
Liam Goodacre <goodacre.liam@gmail.com>
2025-08-16 16:21:13 +0100

    reversible

--- lib/Cats/Profunctor.hs
+++ lib/Cats/Profunctor.hs
@@ -3,32 +3,34 @@ module Cats.Profunctor where
 import Cats.Category
 import Cats.CrossProduct
 import Cats.Functor
+import Data.Kind (Constraint)
+import Data.Type.Equality (type (~))
 
 newtype Viewer :: CATEGORY o -> CATEGORY (o, o) where
-  Viewer :: {runViewer :: k (Fst st) (Fst ab)} -> Viewer k ab st
+  Viewer :: {runViewer :: arr (Fst st) (Fst ab)} -> Viewer arr ab st
 
-type instance o ∈ Viewer k = o ∈ (k × k)
+type instance o ∈ Viewer arr = o ∈ (arr × arr)
 
-instance (Semigroupoid k) => Semigroupoid (Viewer k) where
+instance (Semigroupoid arr) => Semigroupoid (Viewer arr) where
   Viewer f ∘ Viewer g = Viewer (g ∘ f)
 
-instance (Category k) => Category (Viewer k) where
+instance (Category arr) => Category (Viewer arr) where
   identity _ = Viewer (identity _)
 
 data Like :: CATEGORY o -> CATEGORY (o, o) where
   Like ::
-    !(k (Fst st) (Fst ab)) ->
-    !(k (Snd ab) (Snd st)) ->
-    Like k ab st
+    !(arr (Fst st) (Fst ab)) ->
+    !(arr (Snd ab) (Snd st)) ->
+    Like arr ab st
 
-type instance o ∈ Like k = o ∈ (k × k)
+type instance o ∈ Like arr = o ∈ (arr × arr)
 
-instance (Semigroupoid k) => Semigroupoid (Like k) where
+instance (Semigroupoid arr) => Semigroupoid (Like arr) where
   Like f g ∘ Like h i = Like (h ∘ f) (g ∘ i)
 
 type TensoredObjects ::
-  forall o (k :: CATEGORY o).
-  ((k × k) --> k) ->
+  forall o (arr :: CATEGORY o).
+  ((arr × arr) --> arr) ->
   o ->
   (o, o) ->
   (o, o)
@@ -39,8 +41,8 @@ type TensoredObjects tensor e ab =
 
 data
   Tensored ::
-    forall (k :: CATEGORY o).
-    ((k × k) --> k) ->
+    forall (arr :: CATEGORY o).
+    ((arr × arr) --> arr) ->
     CATEGORY (o, o) ->
     CATEGORY (o, o)
   where
@@ -49,3 +51,28 @@ data
     Tensored tensor arr ab st
 
 type instance o ∈ Tensored tensor arr = o ∈ arr
+
+type data Direction = RTL | LTR
+
+type ReverseDirection :: Direction -> Direction
+type family ReverseDirection dir where
+  ReverseDirection RTL = LTR
+  ReverseDirection LTR = RTL
+
+data Glass :: Direction -> CATEGORY (o, o) -> CATEGORY (o, o) where
+  Window :: !(proarr '(a, b) '(s, t)) -> Glass RTL proarr '(a, b) '(s, t)
+  Mirror :: !(proarr '(t, s) '(b, a)) -> Glass LTR proarr '(a, b) '(s, t)
+
+type Reversible :: CATEGORY (o, o) -> CATEGORY (o, o) -> Constraint
+class Reversible input output | input -> output, output -> input where
+  reversed :: input '(a, b) '(s, t) -> output '(t, s) '(b, a)
+
+instance Reversible (Like arr) (Like arr) where
+  reversed (Like sa bt) = Like bt sa
+
+instance
+  (m ~ ReverseDirection w, ReverseDirection m ~ w) =>
+  Reversible (Glass m arr) (Glass w arr)
+  where
+  reversed (Window k) = Mirror k
+  reversed (Mirror k) = Window k
