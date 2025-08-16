e3964bf62a6597399133689e3f35359b23f257de
Liam Goodacre <goodacre.liam@gmail.com>
2025-08-16 16:15:44 +0100

    -parens

--- lib/Cats/Profunctor.hs
+++ lib/Cats/Profunctor.hs
@@ -7,7 +7,7 @@ import Cats.Functor
 newtype Viewer :: CATEGORY o -> CATEGORY (o, o) where
   Viewer :: {runViewer :: k (Fst st) (Fst ab)} -> Viewer k ab st
 
-type instance o ∈ Viewer k = (o ∈ (k × k))
+type instance o ∈ Viewer k = o ∈ (k × k)
 
 instance (Semigroupoid k) => Semigroupoid (Viewer k) where
   Viewer f ∘ Viewer g = Viewer (g ∘ f)
@@ -21,7 +21,7 @@ data Like :: CATEGORY o -> CATEGORY (o, o) where
     !(k (Snd ab) (Snd st)) ->
     Like k ab st
 
-type instance o ∈ Like k = (o ∈ (k × k))
+type instance o ∈ Like k = o ∈ (k × k)
 
 instance (Semigroupoid k) => Semigroupoid (Like k) where
   Like f g ∘ Like h i = Like (h ∘ f) (g ∘ i)
@@ -48,4 +48,4 @@ data
     !(arr (TensoredObjects tensor e ab) st) ->
     Tensored tensor arr ab st
 
-type instance o ∈ Tensored tensor arr = (o ∈ arr)
+type instance o ∈ Tensored tensor arr = o ∈ arr
