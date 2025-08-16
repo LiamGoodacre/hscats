e5a0c50f0c215189653dbd3556c03fe99ad7d7f8
Liam Goodacre <goodacre.liam@gmail.com>
2025-08-16 13:44:51 +0100

    more day

--- lib/Cats/Day.hs
+++ lib/Cats/Day.hs
@@ -1,8 +1,21 @@
-module Cats.Day where
+module Cats.Day
+  ( DataDay (..),
+    Day,
+    bogusLTypes,
+    dayToComposeTypes,
+    composeToDayTypes,
+  )
+where
 
+import Cats.Adjoint
 import Cats.Category
+import Cats.Compose
+import Cats.Delta
+import Cats.Exponential
 import Cats.Functor
 import Cats.Product
+import Data.Function ((&))
+import GHC.Tuple (Unit)
 
 type DataDay ::
   forall d c.
@@ -37,3 +50,22 @@ instance
   where
   map _ dab (DataDayTypes @x @y xyz fx gy) =
     DataDayTypes @x @y (dab ∘ xyz) fx gy
+
+dayToComposeTypes ::
+  forall f g.
+  (Functor f, Functor g) =>
+  Day (∧) f g ~> (f • g)
+dayToComposeTypes = EXP \_ (DataDayTypes xyz fx gy) ->
+  map f (\x -> map g (\y -> xyz (x, y)) gy) fx
+
+bogusLTypes :: forall x y -> Act f x -> Act g y -> Act (Day @Types @Types (∧) f g) y
+bogusLTypes x _ f_ gi = DataDayTypes (\(_ :: x, v) -> v) f_ gi
+
+composeToDayTypes ::
+  forall f g r.
+  (f ⊣ r, Functor g) =>
+  (f • g) ~> Day @Types @Types (∧) f g
+composeToDayTypes = EXP \i ->
+  rightToLeft r f \gi ->
+    () & leftToRight f r \f_ ->
+      bogusLTypes @f @g Unit i f_ gi
