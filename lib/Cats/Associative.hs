6ef35b2a62b7fa2de353a9cbf929671a599f5bb6
Liam Goodacre <goodacre.liam@gmail.com>
2025-11-13 20:44:12 +0000

    Associative OR

--- lib/Cats/Associative.hs
+++ lib/Cats/Associative.hs
@@ -9,6 +9,7 @@ import Cats.Exponential
 import Cats.Functor
 import Data.Kind (Constraint)
 import Data.Type.Equality (type (~))
+import Prelude qualified
 
 type Associative ::
   forall {i}.
@@ -34,6 +35,16 @@ class (Functor op) => Associative (op :: BINARY_OP k) where
 instance Associative (∧) where
   lassoc _ _ _ _ = \(a, (b, c)) -> ((a, b), c)
   rassoc _ _ _ _ = \((a, b), c) -> (a, (b, c))
+
+instance Associative (∨) where
+  lassoc _ _ _ _ = \case
+    Prelude.Left a -> Prelude.Left (Prelude.Left a)
+    Prelude.Right (Prelude.Left b) -> Prelude.Left (Prelude.Right b)
+    Prelude.Right (Prelude.Right c) -> Prelude.Right c
+  rassoc _ _ _ _ = \case
+    Prelude.Left (Prelude.Left a) -> Prelude.Left a
+    Prelude.Left (Prelude.Right b) -> Prelude.Right (Prelude.Left b)
+    Prelude.Right c -> Prelude.Right (Prelude.Right c)
 
 instance (Category k) => Associative (Composing :: BINARY_OP (k ^ k)) where
   lassoc _ _ _ _ = EXP \_ -> identity _
