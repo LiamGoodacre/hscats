2d2eaeb31cff24c4915b91ab8f7924c1e33720d6
Liam Goodacre <goodacre.liam@gmail.com>
2026-07-31 22:40:27 +0100

    boink

--- lib/Cats/Profunctor.hs
+++ lib/Cats/Profunctor.hs
@@ -2,9 +2,10 @@ module Cats.Profunctor where
 
 import Cats.Category
 import Cats.CrossProduct
+import Cats.Delta
 import Cats.Functor
 import Cats.Hom
-import Data.Kind (Constraint)
+import Data.Kind (Constraint, Type)
 import Data.Type.Equality (type (~))
 
 newtype Viewer :: CATEGORY o -> CATEGORY (o, o) where
@@ -33,8 +34,8 @@ instance (Category arr) => Category (Like arr) where
   identity _ = Like (identity _) (identity _)
 
 type TensoredObjects ::
-  forall o (arr :: CATEGORY o).
-  ((arr × arr) --> arr) ->
+  forall o (l :: CATEGORY o) (r :: CATEGORY o) (arr :: CATEGORY o).
+  ((l × r) --> arr) ->
   o ->
   (o, o) ->
   (o, o)
@@ -45,12 +46,12 @@ type TensoredObjects tensor e ab =
 
 data
   Tensored ::
-    forall (arr :: CATEGORY o).
-    ((arr × arr) --> arr) ->
+    forall (l :: CATEGORY o) (r :: CATEGORY o) (arr :: CATEGORY o).
+    ((l × r) --> arr) ->
     CATEGORY (o, o) ->
     CATEGORY (o, o)
   where
-  Tensored ::
+  MkTensored ::
     !(arr (TensoredObjects tensor e ab) st) ->
     Tensored tensor arr ab st
 
@@ -66,6 +67,8 @@ type family ReverseDirection dir where
 data Glass :: Direction -> CATEGORY (o, o) -> CATEGORY (o, o) where
   Window :: !(proarr '(a, b) '(s, t)) -> Glass RTL proarr '(a, b) '(s, t)
   Mirror :: !(proarr '(t, s) '(b, a)) -> Glass LTR proarr '(a, b) '(s, t)
+
+type instance e ∈ Glass d k = e ∈ k
 
 type Reversible :: CATEGORY (o, o) -> CATEGORY (o, o) -> Constraint
 class Reversible input output | input -> output, output -> input where
@@ -130,3 +133,107 @@ instance Functor (InOptic IsoLike (Hom (->))) where
 
 instance Functor (InOptic IsoLike (Cosliced ViewLike xy)) where
   map _ (Window (Like sa _bt)) (Window ar) = Window (Viewer sa ∘ ar)
+
+-- Shapes
+
+type TupleShaped c = Tensored (∧) (Like c)
+
+type EitherShaped c = Tensored (∨) (Like c)
+
+type DomShaped c = Tensored (Hom Types) (Like c)
+
+-- Optic likes
+
+type LensLike = Glass RTL (TupleShaped Types)
+
+type ColensLike = Glass LTR (TupleShaped Types)
+
+type PrismLike = Glass RTL (EitherShaped Types)
+
+type CoprismLike = Glass LTR (EitherShaped Types)
+
+type GrateLike = Glass RTL (DomShaped Types)
+
+type CograteLike = Glass LTR (DomShaped Types)
+
+-- Super instances
+-- ...
+
+-- Aliases
+
+type Optical ::
+  ((l × r) --> Types) ->
+  (NamesOf l -> NamesOf r -> NamesOf l -> NamesOf r -> Type)
+type Optical p a b s t =
+  Act p '(a, b) -> Act p '(s, t)
+
+type OpticOf ::
+  CATEGORY (o, o) ->
+  ((l × r) --> Types) ->
+  (NamesOf l -> NamesOf r -> NamesOf l -> NamesOf r -> Type)
+type OpticOf k p a b s t =
+  (Functor p, Functor (InOptic k p)) =>
+  Optical p a b s t
+
+type Optic ::
+  CATEGORY (o, o) ->
+  (i -> i -> i -> i -> Type)
+type Optic k a b s t =
+  forall l r (p :: (l × r) --> Types).
+  OpticOf k p a b s t
+
+type DataIso = InOptic IsoLike
+
+type DataLens = InOptic LensLike
+
+type DataPrism = InOptic PrismLike
+
+type DataGrate = InOptic GrateLike
+
+type Iso a b s t = Optic IsoLike a b s t
+
+type Lens a b s t = Optic LensLike a b s t
+
+type Prism a b s t = Optic PrismLike a b s t
+
+type Grate a b s t = Optic GrateLike a b s t
+
+iso ::
+  forall p a b s t.
+  (s -> a) ->
+  (b -> t) ->
+  OpticOf IsoLike p a b s t
+iso sa bt =
+  map
+    (DataIso p)
+    (Window (Like sa bt))
+
+lens ::
+  forall p a b s t e.
+  (s -> Act (∧) '(e, a)) ->
+  (Act (∧) '(e, b) -> t) ->
+  OpticOf LensLike p a b s t
+lens sea ebt =
+  map
+    (DataLens p)
+    (Window (MkTensored (Like sea ebt)))
+
+prism ::
+  forall p a b s t e.
+  (s -> Act (∨) '(e, a)) ->
+  (Act (∨) '(e, b) -> t) ->
+  OpticOf PrismLike p a b s t
+prism sea ebt =
+  map
+    (DataPrism p)
+    (Window (MkTensored (Like sea ebt)))
+
+grate ::
+  forall p a b s t e.
+  (s -> Act (Hom Types) '(e, a)) ->
+  (Act (Hom Types) '(e, b) -> t) ->
+  OpticOf GrateLike p a b s t
+grate sea ebt =
+  map
+    (DataGrate p)
+    (Window (MkTensored (Like sea ebt)))
